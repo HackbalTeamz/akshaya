dfa031977d3d6cd84fbac1e30f8148b0e05ea621
Akhil MS <akhilsree001@gmail.com>
2020-12-18 20:48:05 +0530

    add typed effect for our services

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,6 +10,19 @@
     }
   });
 
+if ($('.typed').length) {
+    var typed_strings = $(".typed").data('typed-items');
+    typed_strings = typed_strings.split('-')
+    new Typed('.typed', {
+      strings: typed_strings,
+      loop: true,
+      typeSpeed: 100,
+      backSpeed: 5,
+      backDelay: 2000
+    });
+  }
+
+  
   // Smooth scroll for the navigation menu and links with .scrollto classes
   var scrolltoOffset = $('#header').outerHeight() - 16;
   if (window.matchMedia("(max-width: 991px)").matches) {
